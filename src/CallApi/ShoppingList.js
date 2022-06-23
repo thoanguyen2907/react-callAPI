52251513d91dc1eeb53787c79f4ee8a05de48592
maggiesoni <sonimaggie@gmail.com>
2022-06-23 21:55:26 +0200

    callAPI

--- src/CallApi/ShoppingList.js
+++ src/CallApi/ShoppingList.js
@@ -1,5 +1,27 @@
 import React from "react";
+import axios from "axios"
+import UserList from "./UserList";
+import { useState, useEffect } from "react";
 
 export default function ShoppingList() {
-  return <div>ShoppingList</div>;
+  const [users, setUsers] = useState([])
+  const userInfo= async ()=>{
+    try{
+await axios
+.get("https://api.escuelajs.co/api/v1/users")
+.then((response)=>setUsers(response.data))
+
+}
+catch(error){
+  console.log(error)
+};
+  }
+
+  useEffect(()=>{
+ userInfo()
+  }
+,[])
+
+ 
+  return <UserList usersInformation = {users}/>;
 }
